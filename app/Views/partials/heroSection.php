77c8c4ed2de4391a13f0b667831f78a5f3d73256
Rahman Hakim <rahmanhakim2435@gmail.com>
2025-02-06 00:03:49 +0700

    update parallax

---
+++ app/Views/partials/heroSection.php
@@ -0,0 +1,90 @@
+<!-- Hero Section -->
+<section class="relative w-full h-[80vh] md:h-screen overflow-hidden">
+    <!-- Background -->
+    <div class="absolute inset-0 bg-cover bg-center hero-element transition-transform duration-500"
+         style="background-image: url('assets/images/BG.png'); opacity:1;"></div>
+
+    <!-- Grass -->
+    <img src="assets/images/GRASS.png" alt="Grass"
+    data-direction="bottom"
+         class="absolute -bottom-5 md:-bottom-10 w-full h-auto hero-element transition-transform duration-500 scale-90 md:scale-100" 
+         style="opacity:1;">
+
+    <!-- Cows -->
+    <img src="assets/images/COW_1.png" alt="Cow 1"
+    data-direction="left"
+    id="flipped"
+    class="absolute -bottom-5 -left-20 md:-left-28 w-[300px] md:w-[500px] hero-element transition-transform duration-500 transform translate-x-[-50px] scale-x-100"
+    style="opacity:1;">
+
+
+    <!-- Product -->
+    <img src="assets/images/cheddar-premium.png" alt="Product"
+    data-direction="right"
+    class="absolute w-[250px] h-auto md:w-[410px] md:h-[310px] bottom-[250px] md:bottom-[170px] right-8 md:right-32 hero-element transition-transform duration-500"
+    style="transform: translateX(100vw);">
+
+    <!-- Light Ring -->
+    <img src="assets/images/LIGHT_2.png" alt="Light Ring"
+    data-direction="right"
+    class="absolute bottom-[190px] md:bottom-[90px] right-[-5px] md:right-[72px] w-[250px] h-[250px] md:w-[400px] md:h-[400px] mix-blend-screen hero-element transition-transform duration-500"
+    style="transform: translateX(100vw);">
+
+    <!-- Light Effects -->
+    <img src="assets/images/LIGHT_1.png" alt="Light 1"
+    data-direction="right"
+    class="absolute top-[300px] md:top-[285px] right-[120px] md:right-[240px] w-24 md:w-48 mix-blend-screen hero-element transition-transform duration-500"
+    style="transform: translateX(100vw);">
+
+    <img src="assets/images/LIGHT_3.png" alt="Light 3"
+    data-direction="right"
+    class="absolute top-[340px] md:top-[330px] right-[220px] md:right-[430px] w-32 md:w-64 mix-blend-screen hero-element transition-transform duration-500"
+    style="transform: translateX(100vw);">
+
+    <!-- Vitamins -->
+    <img src="assets/images/VIT_A.png" alt="Vitamin A" 
+    data-direction="right"
+    class="absolute bottom-[375px] md:bottom-[400px] right-[25px] md:right-32 w-12 md:w-16 hero-element transition-transform duration-500" 
+    style="transform: translateX(100vw);">
+
+    <img src="assets/images/VIT_D.png" alt="Vitamin D"
+    data-direction="right"
+    class="absolute bottom-[320px] md:bottom-[340px] right-[2px] md:right-[90px] w-12 md:w-16 hero-element transition-transform duration-500" 
+    style="transform: translateX(100vw);">
+
+    <img src="assets/images/VIT_E.png" alt="Vitamin E" 
+    data-direction="right"
+    class="absolute bottom-[270px] md:bottom-[260px] right-[10px] md:right-24 w-12 md:w-16 hero-element transition-transform duration-500" 
+    style="transform: translateX(100vw);">
+
+    <!-- Cows -->
+    <img src="assets/images/COW_2.png" alt="Cow 2"
+    data-direction="left"
+    class="absolute bottom-[30px] left-[250px] md:bottom-[100px] md:left-[380px] -translate-x-1/2 w-12 md:w-20 hero-element transition-transform duration-500"
+    style="transform: translateX(-50px); opacity:1;">
+
+    <img src="assets/images/COW_3.png" alt="Cow 3"
+    data-direction="left"
+    class="absolute bottom-[50px] left-[320px] md:bottom-[150px] md:left-[450px] w-8 md:w-12 hero-element transition-transform duration-500"
+    style="transform: translateX(-50px); opacity:1;">
+
+    <img src="assets/images/COW_3.png" alt="Cow 3"
+    data-direction="left"
+    class="absolute bottom-[60px] left-[380px] md:bottom-[200px] md:left-[550px] w-4 md:w-4 hero-element transition-transform duration-500"
+    style="transform: translateX(-50px); opacity:1;">
+
+    <!-- Text -->
+    <div data-direction="left"
+         class="absolute top-32 md:top-28 left-4 md:left-9 text-center md:text-right flex flex-col items-center hero-element transition-transform duration-500"
+         style="transform: translateX(-50px); opacity:1;">
+      <h1 class="text-white text-4xl md:text-6xl font-pacifico drop-shadow-lg -rotate-6"
+          style="font-family: 'Dancing Script', cursive">
+        Kebaikan Susu Sapi Asli
+      </h1>
+      <br class="hidden md:block"/>
+      <h1 class="text-white text-4xl md:text-6xl font-bold font-pacifico drop-shadow-lg -rotate-6"
+          style="font-family: 'Libre Baskerville', serif">
+        New Zealand
+      </h1>
+    </div>
+</section>
